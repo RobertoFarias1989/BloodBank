1a38d949d6cab29db3c43fd13f67d8517547d1d8
RobertoFarias1989 <robertosf1989@gmail.com>
2024-05-24 00:50:27 -0300

    wip: added controllers

---
+++ BloodBank.API/Controllers/BloodStocksController.cs
@@ -0,0 +1,72 @@
+﻿using BloodBank.Application.Commands.CreateBloodStock;
+using BloodBank.Application.Commands.DeleteBloodStock;
+using BloodBank.Application.Commands.UpdateBloodStock;
+using BloodBank.Application.Querys.GetAllBloodStocks;
+using BloodBank.Application.Querys.GetBloodStockByIdQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodBank.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BloodStocksController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BloodStocksController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(string? query)
+    {
+        var getAllBloodStocksQuery = new GetAllBloodStocksQuery(query);
+
+        var bloodStock = await _mediator.Send(getAllBloodStocksQuery);
+
+        return Ok(bloodStock);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var query = new GetBloodStockByIdQuery(id);
+
+        var bloodStock = await _mediator.Send(query);
+
+        if(bloodStock == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(bloodStock);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CreateBloodStockCommand command)
+    {
+        await _mediator.Send(command);
+
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, [FromBody] UpdateBloodStockCommand command)
+    {
+        await _mediator.Send(command);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var command = new DeleteBloodStockCommand(id);
+
+        await _mediator.Send(command);
+
+        return NoContent();
+    }
+}
---
+++ BloodBank.API/Controllers/DonationsController.cs
@@ -0,0 +1,74 @@
+﻿using BloodBank.Application.Commands.CreateDonation;
+using BloodBank.Application.Commands.DeleteDonation;
+using BloodBank.Application.Commands.UpdateDonation;
+using BloodBank.Application.Querys.GetAllDonations;
+using BloodBank.Application.Querys.GetDonationById;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodBank.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DonationsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DonationsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(string query)
+    {
+        var getAllDonationsQuery = new GetAllDonationsQuery(query);
+
+        var donations = await _mediator.Send(getAllDonationsQuery);
+
+        return Ok(donations);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var query = new GetDonationByIdQuery(id);
+
+        var donation = await _mediator.Send(query);
+
+        if(donation == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(donation);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CreateDonationCommand command)
+    {
+        await _mediator.Send(command);
+
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, [FromBody] UpdateDonationCommand command)
+    {
+        await _mediator.Send(command);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var command = new DeleteDonationCommand(id);
+
+        await _mediator.Send(command);
+
+        return NoContent();
+    }
+
+}
